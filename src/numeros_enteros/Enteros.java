4c30d9323dc8d27c306d51cb27a7d73de292836c
Sara Thapa Kc <stha827@g.educaand.es>
2024-11-20 10:34:44 +0100

    Añadimos la funcion para que aparezca en tercer numero si esta entre el primero y segundo

--- src/numeros_enteros/Enteros.java
+++ src/numeros_enteros/Enteros.java
@@ -11,9 +11,16 @@ public class Enteros {
 		int numero1 = Integer.parseInt(scanner.nextLine());
 		System.out.println("Escribe el segundo número: ");
 		int numero2 = Integer.parseInt(scanner.nextLine());
+		System.out.println("Escribe el tercer número: ");
+		int numero3 = Integer.parseInt(scanner.nextLine());
 		while (numero1<numero2) {
 			numero1 = numero1+7;
+			if ((numero3<numero2) && (numero3>numero1)) {
 				System.out.println(numero1);
+				System.out.println(numero3);
+			} else {
+				System.out.println(numero1);
+			}
 		}
 		scanner.close();
 	}
