19111e05846eb179058abafc89fee6d21bffeae7
vmemper <vmemper219@g.educaand.es>
2024-11-22 13:27:07 +0100

    Agrego título informatico al método

--- src/numeros_aleatorios/Aleatorio.java
+++ src/numeros_aleatorios/Aleatorio.java
@@ -4,6 +4,7 @@ public class Aleatorio {
 
 	public static void main(String[] args) {
 		int[] aleatorio = new int[100];
+		System.out.println("Número aleatorio");
 		for(int i =0; i<aleatorio.length; i++) {
 			aleatorio[i] = (int) (Math.random()*10);
 			System.out.println(i + ": " + aleatorio[i]);
